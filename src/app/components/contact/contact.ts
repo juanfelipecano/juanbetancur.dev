40acbe133569c0317b5f3469ea4a8dcb2319ef29
Juan Betancur <juanbetancur.dev@gmail.com>
2026-07-12 21:33:03 -0500

    call contact me api

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { TranslatePipe } from '@ngx-translate/core';
+import { ContactMe } from '../../services';
 
 @Component({
     selector: 'jb-contact',
@@ -12,26 +13,52 @@ import { TranslatePipe } from '@ngx-translate/core';
     styleUrl: './contact.scss',
 })
 export class Contact {
-    sent = signal(false);
+    private readonly _contactMeService = inject(ContactMe);
 
-    form = new FormGroup({
+    public isSuccess = signal<boolean | null>(null);
+    public isSending = signal(false);
+
+    public form = new FormGroup({
         name: new FormControl('', { validators: Validators.required, nonNullable: true }),
         email: new FormControl('', { validators: [Validators.required, Validators.email], nonNullable: true }),
         message: new FormControl('', { validators: Validators.required, nonNullable: true }),
     });
+
+    constructor() {
+        effect(() => {
+            if (this.isSending()) {
+                this.form.get('name')?.disable();
+                this.form.get('email')?.disable();
+                this.form.get('message')?.disable();
+            } else {
+                this.form.get('name')?.enable();
+                this.form.get('email')?.enable();
+                this.form.get('message')?.enable();
+            }
+        });
+    }
 
     protected isInvalid(control: 'name' | 'email' | 'message'): boolean {
         const field = this.form.controls[control];
         return field.invalid && field.touched;
     }
 
-    submit() {
+    public async submit(): Promise<void> {
         if (this.form.invalid) return;
 
-        this.sent.set(true);
-        setTimeout(() => {
-            this.sent.set(false);
-            this.form.reset();
-        }, 2200);
+        this.isSending.set(true);
+
+        const data = this.form.value as { name: string; email: string; message: string };
+        const success = await this._contactMeService.perform(data);
+
+        this.isSending.set(false);
+
+        if (!success) {
+            this.isSuccess.set(false);
+            return;
+        }
+
+        this.isSuccess.set(true);
+        this.form.reset();
     }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+    supabase: {
+        projectUrl: 'https://fprqrvdvhubxqxukqfsc.supabase.co',
+        publishableKey: 'sb_publishable_MQ4NeQw29VX8YT4JEhdGrA_qkzTyLds',
+    },
+};
